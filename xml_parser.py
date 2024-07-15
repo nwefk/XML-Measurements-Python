f013128a0dd9039e4f4b018bb6d57fbd141987e8
nwefk <129041852+nwefk@users.noreply.github.com>
2024-07-15 09:26:31 +0200

    Update xml_parser.py

--- xml_parser.py
+++ xml_parser.py
@@ -7,50 +7,57 @@ from tkinter import filedialog
 import os
 
 
-file_name = "test_data_"
-
-root = tk.Tk()
-root.withdraw()
-
-file_path_get = filedialog.askopenfilename(
-    # Ask the user to select which file to use
-    title="Select an XML file", 
-    filetypes=(("XML files", "*.xml"), ("All files", "*.*"))
-    )
-
-if file_path_get:
-    # Ask the user to select the directory to save the files
-    save_directory = filedialog.askdirectory(
-        title="Select directory to save files"
-    )
-
-    if save_directory:
-        tree = ET.parse(file_path_get)
-        root = tree.getroot()
-
-        name_list = []
-        name_count = {}
-
-        # In the case of reoccuring names
-        for names in root.iter("Ydata"):
-            ylabel = names.attrib["ylabel"]
+# In the case of reoccuring names
+def labels(root):
+    label_count = {}
+    label_list = []
+    for names in root.iter("Ydata"):
+        ylabel = names.attrib["ylabel"]
+        
+        if ylabel in label_count:
+            label_count[ylabel] += 1
+            label_list.append(f"{ylabel}_{label_count[ylabel]}")
             
-            if ylabel in name_count:
-                name_count[ylabel] += 1
-                name_list.append(f"{ylabel}_{name_count[ylabel]}")
-                
-            else:
-                name_count[ylabel] = 0
-                name_list.append(ylabel)
+        else:
+            label_count[ylabel] = 0
+            label_list.append(ylabel)
+
+    return label_list
 
 
-        for index, values in enumerate(root.iter("values")):
-            encoded_data = values.text.strip()
-            binary_data = base64.b64decode(encoded_data)
-            num_values = int(values.attrib['numvalues'])
-            float_values = struct.unpack('<' + 'f' * num_values, binary_data)
+def xml_parse():
+    root = tk.Tk()
+    root.withdraw()
 
-            file_path_write = os.path.join(save_directory, f"{file_name}{name_list[index]}.txt")
+    # Ask the user to select which file to use
+    file_path_get = filedialog.askopenfilename(
+        title="Select an XML file", 
+        filetypes=(("XML files", "*.xml"), ("All files", "*.*"))
+        )
+    
+    # Ask the user to select the directory to save the files
+    if file_path_get:
+        save_directory = filedialog.askdirectory(
+            title="Select directory to save files"
+        )
 
-            with open(file_path_write, "w") as file:
-                file.write("\n".join(str(value) for value in float_values[:num_values]))
+        if save_directory:
+            tree = ET.parse(file_path_get)
+            root = tree.getroot()
+
+            name_list = labels(root)
+
+            for index, values in enumerate(root.iter("values")):
+                encoded_data = values.text.strip()
+                binary_data = base64.b64decode(encoded_data)
+                num_values = int(values.attrib['numvalues'])
+                float_values = struct.unpack('<' + 'f' * num_values, binary_data)
+
+                file_path_write = os.path.join(save_directory, f"{name_list[index]}.txt")
+
+                with open(file_path_write, "w") as file:
+                    file.write("\n".join(str(value) for value in float_values[:num_values]))
+
+
+if __name__ == "__main__":
+    xml_parse()
