0213bb55959d4de6741403131c65394b0f330b14
nwefk <129041852+nwefk@users.noreply.github.com>
2024-07-15 09:25:08 +0200

    Update graph_plotting.py

--- graph_plotting.py
+++ graph_plotting.py
@@ -1,25 +1,36 @@
 import matplotlib.pyplot as plt
-import numpy as np
 import tkinter as tk
 from tkinter import filedialog
+import os
 
-root = tk.Tk()
-root.withdraw()
 
-# Ask the user to select which file to use
-file_path = filedialog.askopenfilename(
-    title="Select a file", 
-    filetypes=(("Text files", "*.txt"), ("All files", "*.*"))
-    )
+def plot():
+    root = tk.Tk()
+    root.withdraw()
 
-if file_path:
-    lines_list = []
+    # Ask the user to select which file to use
+    file_path = filedialog.askopenfilename(
+        title="Select a file", 
+        filetypes=(("Text files", "*.txt"), ("All files", "*.*"))
+        )
 
-    with open(file_path, "r") as file:
-        for line in file:
-            lines_list.append(float(line.strip()))
+    if file_path:
+        file_name_with_ext = os.path.basename(file_path)
+        file_name, _ = os.path.splitext(file_name_with_ext)
+        lines_list = []
 
-    ypoints = np.array(lines_list)
+        with open(file_path, "r") as file:
+            for line in file:
+                lines_list.append(float(line.strip()))
 
-    plt.plot(ypoints, linestyle = 'solid')
-    plt.show()
+        fig = plt.gcf()
+        fig.canvas.manager.set_window_title(file_name)
+
+        ypoints = lines_list
+
+        plt.plot(ypoints, linestyle = 'solid')
+        plt.show()
+
+
+if __name__ == "__main__":
+    plot()
